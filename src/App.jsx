3d73d10d3210973bf15cf6a148f7ea7c098b0a56
Prabin Bastakoti <Prabinbastakoti1@gmail.com>
2024-05-09 17:25:01 +0545

    add delivery information

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,21 @@ function App() {
         backgroundPosition: 'center',
       }}
     >
-      <div className=" dark:bg-gray-950 bg-gray-200 opacity-[93%] dark:opacity-[90%]  min-h-screen flex flex-col gap-10 px-6 py-20 font-Montserrat">
+      <div className=" dark:bg-gray-950 bg-gray-200 opacity-[93%] dark:opacity-[90%]  min-h-screen flex flex-col gap-10 px-6 pt-10 pb-20 font-Montserrat">
+        <div className="flex items-center justify-between mb-6">
+          <div>LOGO</div>
+          <div className="text-xs font-semibold flex flex-col gap-2">
+            <div>Delivery Available</div>
+            <div>Delivery Time: 12PM to 2AM</div>
+            <div>
+              <div>Delivery Cost: Rs. 20 - Rs. 40</div>
+              <div className="italic font-medium">
+                (depending on the delivery location)
+              </div>
+            </div>
+            <div>Contact: 9811198594 &nbsp;|&nbsp; 056-000000</div>
+          </div>
+        </div>
         <div className="text-center font-extrabold dark:text-gray-50 text-4xl tracking-wide ">
           MENU
         </div>
