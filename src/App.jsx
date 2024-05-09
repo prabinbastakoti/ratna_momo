f020625ec494df91a58323c792d93fba613e94e4
Prabin Bastakoti <Prabinbastakoti1@gmail.com>
2024-05-09 20:35:18 +0545

    add logo

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,12 @@ function App() {
     >
       <div className=" dark:bg-gray-950 bg-gray-200 opacity-[93%] dark:opacity-[90%]  min-h-screen flex flex-col gap-10 px-6 pt-10 pb-20 font-Montserrat">
         <div className="flex flex-col gap-6 items-center justify-between mb-6">
-          <div>LOGO</div>
+          <div className="dark:hidden mb-3">
+            <img src="/light.png" alt="" />
+          </div>
+          <div className="hidden dark:block mb-3">
+            <img src="/dark.png" alt="" />
+          </div>
           <div className="text-sm font-semibold flex flex-col gap-2 dark:text-gray-100">
             <div>Delivery Available</div>
             <div>Delivery Time: 12PM to 2AM</div>
